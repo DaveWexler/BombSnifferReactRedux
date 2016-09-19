edf96e2f1977ebab6838ef7f3464e18338a08ac0
digitalbuddha <leighscherrer@gmail.com>
2016-09-19 15:05:20 -0400

    movie api calls

--- src/models/movie.js
+++ src/models/movie.js
@@ -8,34 +8,31 @@ class MovieObj {
     this.movieId = movieId
     this.overview = overview
     this.poster = poster
-    // this.youtube = this.getYouTube(title).bind(this)
+    this.youtube = this.getYouTube.bind(this)
     this.getMovieInfo = this.getMovieInfo.bind(this)
-    this.getMovieInfo(movieId)
-
-    this.revenue = attrs[0]
-    this.budget = attrs[1]
-    this.imdb_id = attrs[2]
+    this.getMovieInfo(movieId).then((resp)=>{
+      this.getYouTube(resp.movieId).then(function(resp){
+        this.youTubeURL = resp
+      }.bind(this))
+    })
   }
 
 
 
   getYouTube(title){
-    axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${title.split(" ").join("+")}+trailer&key=AIzaSyDzIKgrZXiQZjPCJT1GcTEggK09QCYESw0`)
+    return axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${title.split(" ").join("+")}+trailer&key=AIzaSyDzIKgrZXiQZjPCJT1GcTEggK09QCYESw0`)
     .then(function(yt){
       return `http://www.youtube.com/embed/${yt.items[0].id.videoId}`
     })
   }
 
   getMovieInfo(movieId){
-    debugger
-    var that = this
     return axios.get(`https://api.themoviedb.org/3/movie/${movieId}?&api_key=bcd69b485671c77289868b4acf21bcf0&append_to_results=imdb_id`)
-    .then((response) => {
-      debugger
-      this.push(response.data.revenue)
-      attrs.push(response.data.budget)
-      attrs.push(response.data.imdb_id)
-      return Promise.all(attrs)
+    .then(function (response){
+      this.revenue = response.data.revenue
+      this.budget = response.data.budget
+      this.imdb_id = response.data.imdb_id
+      return this
     }.bind(this))
   }
 }
